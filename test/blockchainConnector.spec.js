d5f33a04abf097dd306c709c69f0ecf6e68d6648
Hannes Rang <hannes@rang.de>
2018-07-06 23:21:31 +0200

    addind description to test

--- test/blockchainConnector.spec.js
+++ test/blockchainConnector.spec.js
@@ -2,12 +2,16 @@ const blockchainConnector = require("../blockchainConnector");
 const assert = require('assert');
 const fetch = require("node-fetch");
 
-it('should return 0 claims', async function () {
-    
-    const fetchTransactions = blockchainConnector.makeFetchTransactions(fetch);
-    const getClaims = blockchainConnector.makeGetClaims(fetchTransactions);
+describe("blockchainConnector", () => {
+    describe("getClaims", () => {
+        it('should return 0 claims', async function () {
 
-    const claims = await getClaims("___")
-    
-    assert.equal(claims.length, 0);
-});
+            const fetchTransactions = blockchainConnector.makeFetchTransactions(fetch);
+            const getClaims = blockchainConnector.makeGetClaims(fetchTransactions);
+
+            const claims = await getClaims("___")
+
+            assert.equal(claims.length, 0);
+        });
+    })
+})
